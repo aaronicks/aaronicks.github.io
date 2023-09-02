d5a07a6de62f1774ef1cb1dbd54db5b5ea4c22cd
aaronicks <aaronicks@gmail.com>
2023-09-02 11:12:32 +0100

    Nice!

---
+++ jango/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\PYTHRONIX\Desktop\Dev\aaronicks.github.io\jango\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
