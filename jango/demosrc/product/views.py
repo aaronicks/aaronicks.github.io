d39654b876b2d5f422c1e676faad7f1b765749c1
aaronicks <aaronicks@gmail.com>
2023-09-04 10:59:41 +0100

    update

--- jango/demosrc/product/views.py
+++ jango/demosrc/product/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from product.models import Laptop
 # Create your views here.
 def laptop_view(request, *args, **kwargs):
-	print(args, kwargs)
-	return render(request, "laptop.html", {})
+	objt = Laptop.objects.get(id=1)
+	#context = {
+		#"memory":obj.ram,
+		#"graphics": obj.graphics,
+		#"os":obj.os,
+		#"battery":obj.battery
+	#}
+	context = {
+		"obj": objt
+	}
+	return render(request, "laptop.html", context)
 
 
 def about_view(request, *args, **kwargs):
